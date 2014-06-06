01734c91ed986e758d2b6e2ce5cac200e7594306
R. Alex Barbieri <luck@mit.edu>
2014-06-06 09:53:00 +0200

    move to more sensible configuration syntax

--- Configuration/GenProduction/python/Pyquen_EmEnrichedDijet_PtHat170_PartonPt0_ParticlePt35_eta30_TuneZ2_Unquenched_2760GeV_cfi.py
+++ Configuration/GenProduction/python/Pyquen_EmEnrichedDijet_PtHat170_PartonPt0_ParticlePt35_eta30_TuneZ2_Unquenched_2760GeV_cfi.py
@@ -1,5 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
+from Configuration.GenProduction.HI.PyquenTuneZ2Settings_cff import *
+
 hiSignal = cms.EDFilter("PyquenGeneratorFilter",
                         filterType = cms.untracked.string('EcalGenEvtSelector'),
                         partons = cms.vint32(1, 2, 3, 4, 5, 6, #quarks
@@ -38,54 +40,13 @@ hiSignal = cms.EDFilter("PyquenGeneratorFilter",
                         bMax = cms.double(0.0),
                         maxTries = cms.untracked.int32(5000),
                         PythiaParameters = cms.PSet(
-                            myParameters = cms.vstring(),
-                            customProcesses = cms.vstring('MSEL=0   ! User processes'),
-                            ppDefault = cms.vstring('MSEL=1   ! QCD hight pT processes (only jets)',
-                                                    'CKIN(3)=6.',
-                                                    'MSTP(81)=0'),
-                            pythiaUESettingsZ2Tune = cms.vstring(
-                                'MSTU(21)=1     ! Check on possible errors during program execution',
-                                'MSTJ(22)=2     ! Decay those unstable particles',
-                                'PARJ(71)=10 .  ! for which ctau  10 mm',
-                                'MSTP(33)=0     ! no K factors in hard cross sections',
-                                'MSTP(2)=1      ! which order running alphaS',
-                                'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)',
-                                'MSTP(52)=2     ! work with LHAPDF',
-                                'PARP(82)=1.832 ! pt cutoff for multiparton interactions',
-                                'PARP(89)=1800. ! sqrts for which PARP82 is set',
-                                'PARP(90)=0.275 ! Multiple interactions: rescaling power',
-                                'MSTP(95)=6     ! CR (color reconnection parameters)',
-                                'PARP(77)=1.016 ! CR',
-                                'PARP(78)=0.538 ! CR',
-                                'PARP(80)=0.1   ! Prob. colored parton from BBR',
-                                'PARP(83)=0.356 ! Multiple interactions: matter distribution parameter',
-                                'PARP(84)=0.651 ! Multiple interactions: matter distribution parameter',
-                                'PARP(62)=1.025 ! ISR cutoff',
-                                'MSTP(91)=1     ! Gaussian primordial kT',
-                                'PARP(93)=10.0  ! primordial kT-max',
-                                'MSTP(81)=21    ! multiple parton interactions 1 is Pythia default',
-                                'MSTP(82)=4     ! Defines the multi-parton model'),
-                            pythiaPromptPhotons = cms.vstring('MSUB(14)=1',
-                                                              'MSUB(18)=1',
-                                                              'MSUB(29)=1',
-                                                              'MSUB(114)=1',
-                                                              'MSUB(115)=1'),
-                            pythiaJets = cms.vstring('MSUB(11)=1',
-                                                     'MSUB(12)=1',
-                                                     'MSUB(13)=1',
-                                                     'MSUB(28)=1',
-                                                     'MSUB(53)=1',
-                                                     'MSUB(68)=1'),
-                            hydjetPythiaDefault = cms.vstring('MSEL=0   ! user processes',
-                                                              'CKIN(3)=6.',
-                                                              'MSTP(81)=0'),
-                            ppJets = cms.vstring('MSEL=1   ! QCD hight pT processes'),
-                            parameterSets = cms.vstring('pythiaUESettingsZ2Tune',
+                            ppJets = cms.vstring('MSEL=1   ! QCD high pT processes'),
+                            parameterSets = cms.vstring('pythiaUESettings'
                                                         'ppJets',
                                                         'kinematics'),
 
                             kinematics = cms.vstring('CKIN(3)=170',
-                                 'CKIN(4)=9999'
+                                                     'CKIN(4)=9999'
                                                  )
                         )
 )
