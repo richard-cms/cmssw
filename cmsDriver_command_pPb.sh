402a9423d2b1641b94f9e6702d8fd204db3ae708
R. Alex Barbieri <luck@mit.edu>
2014-08-21 13:37:01 -0400

    updates for validation sequence

--- cmsDriver_command_pPb.sh
+++ cmsDriver_command_pPb.sh
@@ -1,37 +1,42 @@
 #!/bin/sh
 
 # First Step is GEN-SIM
-cmsDriver.py Configuration/genproductions/HI/Pyquen_Unquenched_AllQCDPhoton120_PhotonFilter35GeV_eta3_TuneZ2_5020GeV_cfi --filein="dbs:/Hijing_PPb502_MinimumBias/HiWinter13-pa-START53_V10-v1/GEN-SIM" --fileout file:pPb_forward_AllQCDPhoton120_GENSIM.root --eventcontent=RAWSIM --datatier GEN-SIM --step GEN,SIM --conditions  STARTHI53_V27::All --himix --processName HISIGNAL --python_filename cfg1.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 40 --beamspot Match8TeVCollisionPPbBoost
-cmsRun -e -j cfg1_rt.xml cfg1.py
+#specific to forward
+cmsDriver.py Configuration/genproductions/HI/photon_analysis/Pyquen_Unquenched_AllQCDPhoton120_PhotonFilter35GeV_eta3_TuneZ2_pPb_5020GeV_cfi --filein="dbs:/Hijing_PPb502_MinimumBias/HiWinter13-pa-START53_V10-v1/GEN-SIM" --fileout file:pPb_forward_AllQCDPhoton120_GENSIM.root --eventcontent=RAWSIM --datatier GEN-SIM --step GEN,SIM --conditions  STARTHI53_V27::All --himix --processName HISIGNAL --python_filename cfg_forward_1.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 20 --beamspot Match8TeVCollisionPPbBoost
+
+#specific to reverse
+#cmsDriver.py Configuration/genproductions/HI/photon_analysis/Pyquen_Unquenched_AllQCDPhoton120_PhotonFilter35GeV_eta3_TuneZ2_reversepPb_5020GeV_cfi --filein="dbs:/Hijing_PPb502_MinimumBias/HiWinter13-pa-START53_V10-v2/GEN-SIM" --fileout file:pPb_forward_AllQCDPhoton120_GENSIM.root --eventcontent=RAWSIM --datatier GEN-SIM --step GEN,SIM --conditions  STARTHI53_V27::All --himix --processName HISIGNAL --python_filename cfg_forward_1.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 20 --beamspot Match8TeVCollisionPPbBoost --customise_commands 'process.VtxSmeared.Beta=cms.double(0.434)'
+
+cmsRun -e -j cfg_forward_1_rt.xml cfg_forward_1.py
 
 # Second Step is DIGI2RAW
-cmsDriver.py step1 --filein file:pPb_forward_AllQCDPhoton120_GENSIM.root --fileout file:pPb_forward_AllQCDPhoton120_GENSIMRAW.root --eventcontent RAWDEBUG --datatier GEN-SIM-RAW --step DIGI,L1,DIGI2RAW,HLT:PIon --conditions  STARTHI53_V27::All --himix --no_exec --python_filename cfg2.py --customise Configuration/DataProcessing/Utils.addMonitoring -n 40
-cmsRun -e -j cfg2_rt.xml cfg2.py
+cmsDriver.py step1 --filein file:pPb_forward_AllQCDPhoton120_GENSIM.root --fileout file:pPb_forward_AllQCDPhoton120_GENSIMRAW.root --eventcontent RAWDEBUG --datatier GEN-SIM-RAW --step DIGI,L1,DIGI2RAW,HLT:PIon --conditions  STARTHI53_V27::All --himix --no_exec --python_filename cfg_forward_2.py --customise Configuration/DataProcessing/Utils.addMonitoring -n 20
+cmsRun -e -j cfg_forward_2_rt.xml cfg_forward_2.py
 
-# Third Step is RECO
-cmsDriver.py step2 --filein file:pPb_forward_AllQCDPhoton120_GENSIMRAW.root --fileout file:pPb_forward_AllQCDPhoton120RECO.root --eventcontent RECODEBUG --datatier GEN-SIM-RECO --step RAW2DIGI,L1Reco,RECO --conditions STARTHI53_V27::All --himix --no_exec --python_filename cfg3.py --customise Configuration/DataProcessing/Utils.addMonitoring -n 40
-cmsRun -e -j cfg3_rt.xml cfg3.py
+# Third Step is RECO/validation
+cmsDriver.py step2 --filein file:pPb_forward_AllQCDPhoton120_GENSIMRAW.root --fileout file:pPb_forward_AllQCDPhoton120RECO.root --eventcontent RECODEBUG --datatier GEN-SIM-RECO --step GEN:pgen,RAW2DIGI,L1Reco,RECO,VALIDATION:validation_prod,DQM:DQMOfflinePOGMC --customize_commands 'process.genParticles.src="hiSignal"' --conditions STARTHI53_V27::All --himix --no_exec --python_filename cfg_forward_3.py --customise Configuration/DataProcessing/Utils.addMonitoring -n 20
+cmsRun -e -j cfg_forward_3_rt.xml cfg_forward_3.py
 
 echo GEN-SIM step
-grep "TotalEvents" cfg1_rt.xml
-grep "Timing-tstoragefile-write-totalMegabytes" cfg1_rt.xml
-grep "PeakValueRss" cfg1_rt.xml
-grep "AvgEventTime" cfg1_rt.xml
-grep "AvgEventCPU" cfg1_rt.xml
-grep "TotalJobCPU" cfg1_rt.xml
+grep "TotalEvents" cfg_forward_1_rt.xml
+grep "Timing-tstoragefile-write-totalMegabytes" cfg_forward_1_rt.xml
+grep "PeakValueRss" cfg_forward_1_rt.xml
+grep "AvgEventTime" cfg_forward_1_rt.xml
+grep "AvgEventCPU" cfg_forward_1_rt.xml
+grep "TotalJobCPU" cfg_forward_1_rt.xml
 
 echo DIGI2RAW step
-grep "TotalEvents" cfg2_rt.xml
-grep "Timing-tstoragefile-write-totalMegabytes" cfg2_rt.xml
-grep "PeakValueRss" cfg2_rt.xml
-grep "AvgEventTime" cfg2_rt.xml
-grep "AvgEventCPU" cfg2_rt.xml
-grep "TotalJobCPU" cfg2_rt.xml
+grep "TotalEvents" cfg_forward_2_rt.xml
+grep "Timing-tstoragefile-write-totalMegabytes" cfg_forward_2_rt.xml
+grep "PeakValueRss" cfg_forward_2_rt.xml
+grep "AvgEventTime" cfg_forward_2_rt.xml
+grep "AvgEventCPU" cfg_forward_2_rt.xml
+grep "TotalJobCPU" cfg_forward_2_rt.xml
 
-echo RECO step
-grep "TotalEvents" cfg3_rt.xml
-grep "Timing-tstoragefile-write-totalMegabytes" cfg3_rt.xml
-grep "PeakValueRss" cfg3_rt.xml
-grep "AvgEventTime" cfg3_rt.xml
-grep "AvgEventCPU" cfg3_rt.xml
-grep "TotalJobCPU" cfg3_rt.xml
+echo RECO/Validation step
+grep "TotalEvents" cfg_forward_3_rt.xml
+grep "Timing-tstoragefile-write-totalMegabytes" cfg_forward_3_rt.xml
+grep "PeakValueRss" cfg_forward_3_rt.xml
+grep "AvgEventTime" cfg_forward_3_rt.xml
+grep "AvgEventCPU" cfg_forward_3_rt.xml
+grep "TotalJobCPU" cfg_forward_3_rt.xml
