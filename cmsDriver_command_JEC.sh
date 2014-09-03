4fba1852568a9fdf17bcb8dac1a5d092a089d48f
R. Alex Barbieri <luck@mit.edu>
2014-09-03 12:48:40 -0400

    beamspot only needed for GEN-SIM

--- cmsDriver_command_JEC.sh
+++ cmsDriver_command_JEC.sh
@@ -7,13 +7,13 @@ cmsRun -e -j cfg_JEC_1_rt.xml cfg_JEC_1.py
 
 # Second Step is DIGI2RAW
 # RAWDEBUG
-cmsDriver.py step1 --filein file:JEC_GENSIM.root --fileout file:JEC_GENSIMRAW.root --eventcontent RAWDEBUG --datatier GEN-SIM-RAW --step DIGI,L1,DIGI2RAW,HLT:HIon --conditions STARTHI53_LV1::All --scenario HeavyIons --beamspot RealisticHI2011Collision --no_exec --python_filename cfg_JEC_2.py --customise Configuration/DataProcessing/Utils.addMonitoring -n 10
+cmsDriver.py step1 --filein file:JEC_GENSIM.root --fileout file:JEC_GENSIMRAW.root --eventcontent RAWDEBUG --datatier GEN-SIM-RAW --step DIGI,L1,DIGI2RAW,HLT:HIon --conditions STARTHI53_LV1::All --scenario HeavyIons --no_exec --python_filename cfg_JEC_2.py --customise Configuration/DataProcessing/Utils.addMonitoring -n 10
 
 cmsRun -e -j cfg_JEC_2_rt.xml cfg_JEC_2.py
 
 # Third Step is RECO
 #RECODEBUG
-cmsDriver.py step2 --filein file:JEC_GENSIMRAW.root --fileout file:JEC_RECO.root --eventcontent RECODEBUG --datatier GEN-SIM-RECO --step RAW2DIGI,L1Reco,RECO --conditions STARTHI53_LV1::All --scenario HeavyIons --beamspot RealisticHI2011Collision --no_exec --python_filename cfg_JEC_3.py --customise Configuration/DataProcessing/Utils.addMonitoring -n 10 --inputCommands "keep *,drop *_g4SimHits_*_*"
+cmsDriver.py step2 --filein file:JEC_GENSIMRAW.root --fileout file:JEC_RECO.root --eventcontent RECODEBUG --datatier GEN-SIM-RECO --step RAW2DIGI,L1Reco,RECO --conditions STARTHI53_LV1::All --scenario HeavyIons --no_exec --python_filename cfg_JEC_3.py --customise Configuration/DataProcessing/Utils.addMonitoring -n 10 --inputCommands "keep *,drop *_g4SimHits_*_*"
 
 cmsRun -e -j cfg_JEC_3_rt.xml cfg_JEC_3.py
 
