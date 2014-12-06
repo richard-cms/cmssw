e2b6a53c3de8cb766a45a00427a4703e76ed5724
ginnocen <ginnocen@cern.ch>
2014-12-06 13:34:27 +0100

    change of input files

--- EventFilter/L1TRawToDigi/test/test_MP7BufferDumpToRaw_Stage1.py
+++ EventFilter/L1TRawToDigi/test/test_MP7BufferDumpToRaw_Stage1.py
@@ -77,8 +77,10 @@ process.TFileService.fileName = cms.string('l1t.root')
 process.load('EventFilter.L1TRawToDigi.stage1MP7BufferRaw_cfi')
 process.stage1Raw.nFramesOffset    = cms.untracked.int32(0)
 process.stage1Raw.nFramesLatency   = cms.untracked.int32(90)
-process.stage1Raw.rxFile = cms.untracked.string("stage1_rx_summary.txt")
-process.stage1Raw.txFile = cms.untracked.string("stage1_tx_summary.txt")
+process.stage1Raw.rxFile = cms.untracked.string("test/rx_summary.txt")
+process.stage1Raw.txFile = cms.untracked.string("test/tx_summary.txt")
+
+
 
 # dump raw data
 process.dumpRaw = cms.EDAnalyzer( 
